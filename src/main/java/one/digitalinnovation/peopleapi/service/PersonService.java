ce94e05616ecc2f171754caeefd6ffd5e710e03c
jnnmnz <jenifercarvalho_@hotmail.com>
2021-07-26 14:55:42 -0300

    mapping of list of people

--- src/main/java/one/digitalinnovation/peopleapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/peopleapi/service/PersonService.java
@@ -7,8 +7,12 @@ import one.digitalinnovation.peopleapi.mapper.PersonMapper;
 import one.digitalinnovation.peopleapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -27,5 +31,12 @@ public class PersonService {
                 .builder()
                 .message("Creation of person ID #"+savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }

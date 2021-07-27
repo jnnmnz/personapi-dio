2920377bc1947cccc1ff43b331122eda14a226e9
jnnmnz <jenifercarvalho_@hotmail.com>
2021-07-27 12:38:47 -0300

    developed update_by_id feature

--- src/main/java/one/digitalinnovation/peopleapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/peopleapi/service/PersonService.java
@@ -29,10 +29,7 @@ public class PersonService {
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
         Person savedPerson = personRepository.save(personToSave);
-        return MessageResponseDTO
-                .builder()
-                .message("CREATED Person ID "+savedPerson.getId())
-                .build();
+        return createMessageResponse(savedPerson.getId(), "CREATED Person ID ");
     }
 
     public List<PersonDTO> listAll() {
@@ -47,12 +44,27 @@ public class PersonService {
         return personMapper.toDTO(person);
     }
 
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+        personRepository.deleteById(id);
+    }
+
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExists(id);
+
+        Person personToUpdate = personMapper.toModel(personDTO);
+        Person savedPerson = personRepository.save(personToUpdate);
+        return createMessageResponse(savedPerson.getId(), "UPDATED Person ID ");
+    }
+
     private Person verifyIfExists(Long id) throws PersonNotFoundException {
         return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
     }
 
-    public void delete(Long id) throws PersonNotFoundException {
-        verifyIfExists(id);
-        personRepository.deleteById(id);
+    private MessageResponseDTO createMessageResponse(Long id, String msg) {
+        return MessageResponseDTO
+                .builder()
+                .message(msg + id)
+                .build();
     }
 }
